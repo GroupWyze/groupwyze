f708fc93897bcb48f6d33128da2e2aea00fa9970
ikkanlaz <andrew.lazenby@gmail.com>
2017-09-28 23:07:52 -0700

    fixing messaging

--- routes/api/shindigs/users/index.js
+++ routes/api/shindigs/users/index.js
@@ -3,7 +3,7 @@ const shindigUserRoutes = require("./users");
 
 router.param(':user_id', function (req, res, next, id) {
   //TODO: determin if user has access to this  (i.e. event-user id associated with user id)
-  console.log("overall category verification?");
+  console.log("overall user verification?");
   next();
 });
 
