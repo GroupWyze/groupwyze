841f58aa3fc63444eb463f555870056187cb0d28
ikkanlaz <andrew.lazenby@gmail.com>
2017-10-07 06:40:53 -0700

    fixing color on Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,11 +7,11 @@ import FlatButton from 'material-ui/FlatButton';
 // import Toggle from 'material-ui/Toggle';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import ActionHome from 'material-ui/svg-icons/action/home';
-import {red500} from 'material-ui/styles/colors';
+import { red500 } from 'material-ui/styles/colors';
 
 class Login extends Component {
   static muiName = 'FlatButton';
-  
+
   render() {
     return (
       <FlatButton {...this.props} label="Login" />
@@ -29,9 +29,9 @@ const Logged = (props) => (
     anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
   >
 
-    <MenuItem primaryText="My Page" onClick={()=>props.goToUser()}/>
-    <MenuItem primaryText="Create Shindig" onClick={()=>props.goToShindig()}/>
-    <MenuItem primaryText="Sign out" onClick={()=>props.logout()} />
+    <MenuItem primaryText="My Page" onClick={() => props.goToUser()} />
+    <MenuItem primaryText="Create Shindig" onClick={() => props.goToShindig()} />
+    <MenuItem primaryText="Sign out" onClick={() => props.logout()} />
   </IconMenu>
 );
 
@@ -42,7 +42,7 @@ Logged.muiName = 'IconMenu';
  * to render different components depending on the application state.
  */
 class Navbar extends Component {
-  
+
   state = {
 
     logged: this.props.isAuthenticated()
@@ -55,13 +55,15 @@ class Navbar extends Component {
       <div>
         <AppBar
           title="GroupWyze"
-          iconElementLeft={<IconButton><ActionHome onClick={()=>this.props.goToHome()}/></IconButton>}
-          iconElementRight={this.state.logged ? 
+          iconElementLeft={<IconButton><ActionHome onClick={() => this.props.goToHome()} /></IconButton>}
+          iconElementRight={this.state.logged ?
             <Logged logout={this.props.logout}
-                    goToUser={this.props.goToUser}
-                    goToShindig={this.props.goToShindig}
-            /> : <Login onClick={()=>this.props.login()} />}
-
+              goToUser={this.props.goToUser}
+              goToShindig={this.props.goToShindig}
+            /> : <Login onClick={() => this.props.login()} />}
+          style={{
+            backgroundColor: red500,
+          }}
         />
       </div>
     );
