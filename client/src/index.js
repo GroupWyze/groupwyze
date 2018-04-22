5a5ee3bde275b0dc8e637e349d67290d94cb6fed
ikkanlaz <andrew.lazenby@gmail.com>
2018-04-21 19:21:15 -0700

    removing routes file

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Route, Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { makeMainRoutes } from './routes';
+import App from './App';
+import Callback from './pages/Callback/Callback';
+import Auth from './Auth/Auth';
+import history from './history';
+import './index.css';
 
-const routes = makeMainRoutes();
+const auth = new Auth();
+
+const handleAuthentication = ({ location }) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
 
 ReactDOM.render(
-  routes,
+  <Router history={history}>
+    <div>
+      <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+      <Route path="/callback" render={(props) => {
+        handleAuthentication(props);
+        return <Callback {...props} />
+      }} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
 
