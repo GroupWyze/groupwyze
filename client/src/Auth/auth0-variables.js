af6fb029a3babd669ceb1c01b02e90f941639d2d
ikkanlaz <andrew.lazenby@gmail.com>
2018-04-22 08:55:22 -0700

    updating callbackURL

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'groupwyze.auth0.com',
   clientId: 'h-db3TKF59gFCVMNPSxO02CmPvQHL9Nq',
-  callbackUrl: `${process.env.PUBLIC_URL}/callback`
+  callbackUrl: 'https://groupwyze.herokuapp.com/callback'
 }
