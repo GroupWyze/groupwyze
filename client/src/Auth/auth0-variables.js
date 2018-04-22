5b09eb2b309b30a016c358128fd7bd40622e7af1
ikkanlaz <andrew.lazenby@gmail.com>
2018-04-22 08:12:23 -0700

    changing callback url

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'groupwyze.auth0.com',
   clientId: 'h-db3TKF59gFCVMNPSxO02CmPvQHL9Nq',
-  callbackUrl: '/callback'
+  callbackUrl: 'http://www.groupwyze.com/callback'
 }
