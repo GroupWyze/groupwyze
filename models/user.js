aead1b4a057f2d8c4908006a21136f75f6be32ca
ikkanlaz <andrew.lazenby@gmail.com>
2018-05-09 09:22:40 -0700

    removing auth0Id

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ module.exports = function (sequelize, DataTypes) {
     // ID, Name, email, username, date_created, date_deleted
 
     let User = sequelize.define("User", {
-        auth0Id: { type: DataTypes.STRING, allowNull: false },
         firstName: { type: DataTypes.STRING, allowNull: true },
         lastName: { type: DataTypes.STRING, allowNull: true },
         nickname: { type: DataTypes.STRING, allowNull: true },
